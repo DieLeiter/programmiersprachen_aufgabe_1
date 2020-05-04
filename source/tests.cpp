c09a09780a1909460b9388eae39514702e5df86f
TonyZoeppig <tonyjan98@gmail.com>
2020-05-04 16:52:52 +0200

    Aufgabe 1.9, Tests passed

--- source/tests.cpp
+++ source/tests.cpp
@@ -13,7 +13,7 @@ int gcd(int a, int b) {
             max = b;
         }
 
-        for (int i = 2; i <= max; i++) {
+        for (int i = max; i >= 2; i--) {
             if (a % i == 0 && b % i == 0) {
                 return i;
             }
@@ -22,13 +22,46 @@ int gcd(int a, int b) {
     return 1;
 }
 
+int checksum(int a) {
+    int quersumme = 0;
+
+    if (a < 0) {
+        a *= -1;
+    }
+
+    while (a > 0) {
+        quersumme += a % 10;
+        a /= 10;
+    }
+
+    return quersumme;
+}
+
+int sum_multiples() {
+    int sum = 0;
+
+    for (int i = 3; i <= 1000; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+
+    return sum;
+}
+
+TEST_CASE("describe_checksum", "[checksum]") {
+    REQUIRE(checksum(121618) == 19);
+    REQUIRE(checksum(123) == 6);
+    REQUIRE(checksum(0) == 0);
+    REQUIRE(checksum(-123) == 6);
+}
+
 TEST_CASE("describe_gcd", "[gcd]") { 
     REQUIRE(gcd(2, 4) == 2);
     REQUIRE(gcd(9, 6) == 3);
     REQUIRE(gcd(3, 7) == 1);
     REQUIRE(gcd(0, 7) == 1);
 }
-
 
 int main(int argc, char* argv[])
 {
